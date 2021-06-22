22d9d4036c0c887d871f2e5cd08f43c9d52b737e
allentrandinh <anhpidinh@gmail.com>
2021-06-22 15:49:04 -0500

    test post method

--- node/python-scripts/printout.py
+++ node/python-scripts/printout.py
@@ -1,7 +1,9 @@
 import sys
-# import json
+import json
 
-print(sys.argv[1])
-# base_info = json.loads(sys.argv[1])
+# input = sys.argv[1]
+base_info = json.loads(sys.argv[1])
+print(base_info)
+print(type(base_info))
 # print(base_info)
 sys.stdout.flush()
